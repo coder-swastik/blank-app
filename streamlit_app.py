01f80d93a706a1d808d2573258361f9ffefa3994
Swastik Karki <87107603+coder-swastik@users.noreply.github.com>
2025-01-13 15:24:11 +0000

    Form fill area defined

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,32 @@
 import streamlit as st
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+st.title("Please fill the form below")
+
+name = st.text_input("Name")
+email = st.text_input("Email")
+address = st.text_input("Address")
+phone_number = st.text_input("Phone Number")
+
+
+col1 ,col2=st.columns(2)
+
+    
+with col1:
+    st.radio(
+        'Set selectbox label visibility',
+        key='visibility',
+        options=['red','blue','yellow']
+        )
+with col2:
+    option = st.selectbox(
+        "Gender: ",
+        ("Male", "Female", "Other"),
+        index=0,
+        placeholder="Select what you like to be called...",
+        )
+
+if st.button('Submit'):
+    if not name or not email or not address or not phone_number:
+        st.error("Please fill the form")
+    else:
+        st.success("Form Submitted")
