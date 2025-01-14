e6856931b0ccb14e7d146b2b9e23b2d695bb0c3d
Swastik Karki <87107603+coder-swastik@users.noreply.github.com>
2025-01-14 16:18:10 +0000

    Update: Databse connection done and table created to hold data sucessfully

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import sqlite3
 
 st.title("Please fill the form below")
 
@@ -12,21 +13,36 @@ col1 ,col2=st.columns(2)
 
     
 with col1:
-    st.radio(
-        'Set selectbox label visibility',
+   course = st.radio(
+        'Course: ',
         key='visibility',
-        options=['red','blue','yellow']
+        options=['BE. Computer' ,'Bsc CSIT','BCA','BIM','BE. ICT','BCIS' ]
         )
 with col2:
-    option = st.selectbox(
+    gender = st.selectbox(
         "Gender: ",
         ("Male", "Female", "Other"),
         index=0,
         placeholder="Select what you like to be called...",
         )
 
+
+def insert_data(name, email, address, phone_number, course, gender):
+    conn = sqlite3.connect("form_data.db")
+    cursor = conn.cursor()
+    cursor.execute(
+        """
+        INSERT INTO form_entries (name, email, address, phone_number, course, gender)
+        VALUES (?, ?, ?, ?, ?, ?)
+        """,
+        (name, email, address, phone_number, course, gender),
+    )
+    conn.commit()
+    conn.close()
+
 if st.button('Submit'):
     if not name or not email or not address or not phone_number:
         st.error("Please fill the form")
     else:
+        insert_data(name, email, address, phone_number, course, gender)
         st.success("Form Submitted")
