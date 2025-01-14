99b587f6863dbc82a8115bda18dcd6ecb2e8802b
Swastik Karki <87107603+coder-swastik@users.noreply.github.com>
2025-01-14 16:34:15 +0000

    Update: viewing of data store in the database

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,25 @@
 import streamlit as st
 import sqlite3
 
+conn = sqlite3.connect('form_data.db')
+
+cursor = conn.cursor()
+
+cursor.execute('''
+CREATE TABLE IF NOT EXISTS form_entries (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT NOT NULL,
+    email TEXT NOT NULL,
+    address TEXT NOT NULL,
+    phone_number TEXT NOT NULL,
+    course TEXT NOT NULL,
+    gender TEXT NOT NULL
+)
+''')
+
+conn.commit()
+conn.close()
+
 st.title("Please fill the form below")
 
 name = st.text_input("Name")
